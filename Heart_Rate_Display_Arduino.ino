3810260c815df2c27a2ece39165aaed1e1cc884d
Sanjay Ponnambalam <113366933+Sanjayponnambalam@users.noreply.github.com>
2023-09-12 20:36:24 +0530

    Add files via upload

---
+++ Heart_Rate_Display_Arduino.ino
@@ -0,0 +1,39 @@
+/******************************************************************************
+Heart_Rate_Display.ino
+Demo Program for AD8232 Heart Rate sensor.
+
+The AD8232 Heart Rate sensor is a low cost EKG/ECG sensor.  This example shows
+how to create an ECG with real time display.  The display is using Processing.
+This sketch is based heavily on the Graphing Tutorial provided in the Arduino
+IDE. http://www.arduino.cc/en/Tutorial/Graph
+
+
+Development environment specifics:
+	IDE: Arduino 1.0.5
+	Hardware Platform: Arduino Pro 3.3V/8MHz
+	AD8232 Heart Monitor Version: 1.0
+
+
+
+******************************************************************************/
+
+void setup() {
+  // initialize the serial communication:
+  Serial.begin(9600);
+  pinMode(10, INPUT); // Setup for leads off detection LO +
+  pinMode(11, INPUT); // Setup for leads off detection LO -
+
+}
+
+void loop() {
+  
+  if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
+    Serial.println('!');
+  }
+  else{
+    // send the value of analog input 0:
+      Serial.println(analogRead(A0));
+  }
+  //Wait for a bit to keep serial data from saturating
+  delay(1);
+}
